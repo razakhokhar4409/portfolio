52baced5f92e2a80ff12c53a3452840034b5da28
Ali hasnain <ali.hasnain@ascertia.com>
2025-05-05 13:56:49 +0500

    edu details

--- developer-portfolio-main/utils/data/educations.js
+++ developer-portfolio-main/utils/data/educations.js
@@ -2,19 +2,8 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2016 - 2020",
+    institution: "The Superior University, Lahore, Pakistan",
   },
-  {
-    id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
+ 
 ]
