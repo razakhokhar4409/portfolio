c36691c4cf692647ff0ecfc882b7ff36fde3e295
Ali hasnain <ali.hasnain@ascertia.com>
2025-05-05 13:53:27 +0500

    projects data

--- developer-portfolio-main/utils/data/contactsData.js
+++ developer-portfolio-main/utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'ali.hasnain.9279@gmail.com',
+    phone: '+92 323 4693230',
+    address: 'Lahore, Pakistan - 54000',
+    github: 'https://github.com/alihasnain1997',
+    // facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/ali-h-211256135/',
+    // twitter: 'https://twitter.com/said7388',
+    // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "ali hasnain",
 }
