dd9b0188755c24566d97583caf99a88590f093d7
Ali hasnain <ali.hasnain@ascertia.com>
2025-05-05 15:46:18 +0500

    changed to lottie react

--- developer-portfolio-main/app/components/helper/animation-lottie.jsx
+++ developer-portfolio-main/app/components/helper/animation-lottie.jsx
@@ -1,21 +1,16 @@
-"use client"
+'use client';
 
-// import Lottie from "lottie-react";
 import dynamic from 'next/dynamic';
-const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
-
+const AnimationLottie = ({ animationPath, width = '95%' }) => {
   return (
-    <Lottie {...defaultOptions} />
+    <Lottie
+      animationData={animationPath}
+      loop
+      autoplay
+      style={{ width }}
+    />
   );
 };
 
