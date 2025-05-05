58911b2589dabe17e1305c3f281028d3f880e4c6
Ali hasnain <ali.hasnain@ascertia.com>
2025-05-05 12:13:06 +0500

    initial commit

---
+++ developer-portfolio-main/utils/data/personal-data.js
@@ -0,0 +1,22 @@
+export const personalData = {
+  name: "Ali Hasnain",
+  profile: "/profile.png",
+  designation: "Software Developer",
+  description: `My name is Ali Hasnain, and I’m a dedicated and detail-oriented Senior Software Engineer with a strong passion for building scalable and efficient web applications. With hands-on experience across a variety of domains—including medical, banking, ERPs, and CRMs—I thrive in fast-paced environments and enjoy solving complex problems with clean, maintainable code. I'm a quick learner with a methodical, self-driven approach, and I constantly seek opportunities to grow by exploring new technologies and frameworks.
+
+While I’ve worked across the full stack, my expertise lies in JavaScript/TypeScript, particularly with Node.js, NestJS, React.js, and Angular, and I have a deep understanding of AWS serverless architectures, Docker, Redis, PostgreSQL, and real-time systems. I enjoy turning ideas into robust backend systems and user-friendly interfaces, making meaningful contributions to both product and team growth.
+
+I'm always open to exciting opportunities that challenge me and align with my technical skills and passion for software development.`,
+  email: "ali.hasnain.9279@gmail.com",
+  phone: "+92 323 4693230",
+  address: "Lahore, Pakistan - 54000",
+  github: "https://github.com/alihasnain1997",
+  // facebook: "https://www.facebook.com/abusaid.riyaz/",
+  linkedIn: "https://www.linkedin.com/in/ali-h-211256135/",
+  // twitter: "https://twitter.com/said7388",
+  // stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
+  // leetcode: "https://leetcode.com/said3812/",
+  devUsername: "ali hasnain",
+  resume:
+    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+};
